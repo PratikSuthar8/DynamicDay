dfd4b51791195036fd62fd1e320063f4d9602159
Pratik Suthar <d365technical@dynamicdaytechnologies.com>
2024-05-02 18:10:16 +0530

    Add: routes adde to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
+import Solutions from "./Pages/Solutions";
+import Blogs from "./Pages/Blogs";
+import Services from "./Pages/Services";
 
 function App() {
 	return (
 		<>
-			<Home />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/solution" element={<Solutions />} />
+				<Route path="/blog" element={<Blogs />} />
+				<Route path="/services" element={<Services />} />
+			</Routes>
 		</>
 	);
 }
