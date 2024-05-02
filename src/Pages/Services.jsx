f6cf56dd116c8bc412f1c6f7b363018ecf4d2dbe
Pratik Suthar <d365technical@dynamicdaytechnologies.com>
2024-05-02 18:07:02 +0530

    Add: new pages for solution, blogs, services

---
+++ src/Pages/Services.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+Navbar;
+
+const Services = () => {
+	return (
+		<div>
+			<Navbar />
+			<h1>our services</h1>
+		</div>
+	);
+};
+
+export default Services;
---
+++ src/Pages/Solutions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+const Solutions = () => {
+	return (
+		<div>
+			<Navbar />
+			<h1>our solutions</h1>
+		</div>
+	);
+};
+
+export default Solutions;
